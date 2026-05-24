edaf93dd7833fa5a2c1043b847778c1c318cf81f
kakgonbri <buikhacbin@gmail.com>
2026-05-24 23:23:58 +0700

    syntax fix

--- Models/Dto/HostRegistrationDto.cs
+++ Models/Dto/HostRegistrationDto.cs
@@ -6,7 +6,7 @@
         public bool Approved { get; set; }
         public string? Note { get; set; }
         public string UserName { get; set; } = default!;
-        public string? ApprovedBy { get; set; };
+        public string? ApprovedBy { get; set; }
         public string? UserAvatarUrl { get; set; }
         public DateTime? ApprovedOn { get; set; }
         public DateTime CreatedOn { get; set; }
